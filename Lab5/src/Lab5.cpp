a3e88a82c2620e80a2fc90ad1a4479393db9fa10
rebeca <rebeca.currie@ryerson.ca>
2015-10-21 16:39:46 -0400

    Added a new line to the project

---
+++ Lab5/src/Lab5.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Lab5.cpp
+// Author      : Rebeca Currie
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout<<"My name is Rebeca"<<endl;
+	return 0;
+}
